9043a22a5788f796223d9b4cda03c7a9e9838199
Albin Biju <albin.biju@experionglobal.local>
2024-03-04 15:13:51 +0530

    feat: added axiosconfig

---
+++ src/components/InitiateTransferForm/types/index.tsx
@@ -0,0 +1,15 @@
+export interface Employee {
+  id: number;
+  employee_number: string;
+  name: string;
+  mail_id: string;
+  designation: string;
+  // Define other properties if available
+}
+
+export interface Du {
+  id: number;
+  du_name: string;
+}
+
+export type SetEmployeeDataFunction = (data: Employee[]) => void;
