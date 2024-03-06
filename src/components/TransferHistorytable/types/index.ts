69726e0ae3455a940516e2fe11cf0eabd71a85f4
Jitty Tresa Thomas <jittytresa@gmail.com>
2024-03-06 17:21:18 +0530

    add: added sorting function to pending approvals table fix: modified the status column in transfer history

--- src/components/TransferHistorytable/types/index.ts
+++ src/components/TransferHistorytable/types/index.ts
@@ -3,16 +3,20 @@ import { PaginationProps } from "antd";
 export type HandlePaginationChangeType = (newPagination: PaginationProps) => void;
 
 export type dataSourceType = {
-    currentdu: string
+    currentdu: {
+        du_name: string
+    }
     employee:  {
         number: string,
         name: string,
         id: number
     }
     id: number
-    status: number
-    targetdu: string
-    transfer_date: string
+    status: string
+    targetdu: {
+        du_name: string
+    }
+    transfer_date: string;
 }
 
 export type TransferHistoryTablePropsType = { 
