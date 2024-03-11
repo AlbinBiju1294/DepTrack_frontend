af6d4bd8d6875b6b7ca5848775d7d8793ef0cc50
Abin George <abin.george@experionglobal.local>
2024-03-11 12:13:45 +0530

    feat: Added Employee Upload and change Du buttons to Manage Du page

--- src/components/AdminTable/api/fetchAdmindata.ts
+++ src/components/AdminTable/api/fetchAdmindata.ts
@@ -1,14 +1,13 @@
 import axiosInstance from "../../../config/AxiosConfig";
 import { AdminTableSetDataType } from "../types";
 
+//Function to fetch delivery units and their heads
 export const fetchDeliveryUnitData = async (
   setAdminData: AdminTableSetDataType
 ) => {
   try {
     const res = await axiosInstance.get("api/v1/employee/list-duheads/");
     const responseData = res.data.data;
-    console.log(responseData);
-    console.log("DU and DU heads ", responseData.results);
     setAdminData(responseData);
   } catch (error) {
     console.error("Error:", error);
