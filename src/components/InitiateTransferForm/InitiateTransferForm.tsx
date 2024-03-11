06754272869dda30d9d67c5999e9cf7f05e7f2c7
Albin Biju <albin.biju@experionglobal.local>
2024-03-11 11:33:13 +0530

    test: tested latest changes

--- src/components/InitiateTransferForm/InitiateTransferForm.tsx
+++ src/components/InitiateTransferForm/InitiateTransferForm.tsx
@@ -23,6 +23,32 @@ const InitiateTransferForm = ({
   handleDuDropdownChange,
   handleCheckboxChange,
 }: InitiateTransferFormPropsType) => {
+
+  function getCurrentDate() {
+    const today = new Date();
+    let day = today.getDate();
+    console.log(day)
+    let month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    let newday;
+    let newmonth;
+  
+    if (day < 10) {
+      newday = '0' + day.toString();
+    }
+    else{
+      newday = day.toString();
+    }
+    if (month < 10) {
+      newmonth = '0' + month.toString();
+    }
+  
+    return `${year}-${newmonth}-${newday}`;
+  }
+
+  let currentDate = getCurrentDate()
+  console.log(currentDate)
+
   return (
     <>
       {contextHolder}
@@ -81,6 +107,7 @@ const InitiateTransferForm = ({
                 required
                 type="date"
                 name="transfer_date"
+                min={currentDate}
                 onChange={(e) => {
                   handleInputChange(e);
                 }}
